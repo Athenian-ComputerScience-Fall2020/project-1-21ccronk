a5939310c02d2e0d511d403fe571a2facbc2af6a
21ccronk <21ccronk@athenian.org>
2020-10-06 20:58:13 +0000

    Getting there

--- my_code.py
+++ my_code.py
@@ -3,28 +3,29 @@
 
 print("Welcome to the Pizza Shack! Every pizza is served with tomato sauce and cheese, but you can add toppings on top of that, as you'd like, and as your money permits!")
 print("Today's add-on toppings are pepperoni, sausage, olives, mushrooms, bell pepper, and extra cheese!")
-x = int(input("How much money do you have to spend?: ")
+x = int(input("How much money do you have to spend?: "))
 
-while y <= x or count < 6:
-    y = 4
+
+toppings = []
+y = 4
+while y <= x: 
     topping = input("Enter a topping you would like or type 'order' to submit your order: ")
-    if topping == 'pepperoni':
-        y = y + 1.50
-    if topping == 'sausage':
-        y = y + 1.50
-    if topping == 'olives':
-        y = y + .50
-    if topping == 'mushrooms':
-        y = y + .50
-    if topping == 'bell pepper':
-        y = y + .50
-    if topping == 'extra cheese':
-        y = y + 1
-    if topping == 'order':
-        exit loop 
-
-
+    toppings.append(topping)
     
-
-
+    if topping == 'sausage' or topping == 'pepperoni':
+        y = y + 1.50
+        if y > x:
+            print("You don't have enough money to afford this topping.")
+    elif topping == 'olives' or topping == 'bell pepper' or topping == 'mushrooms':
+        y = y + .50
+        if y > x:
+            print("You don't have enough money to afford this topping.")
+    elif topping == 'extra cheese':
+        y = y + 1.00
+        if y > x:
+            print("You don't have enough money to afford this topping.")
+    elif topping == 'order':
+        break
+    
+print("Your pizza will cost $" + str(y))
 
