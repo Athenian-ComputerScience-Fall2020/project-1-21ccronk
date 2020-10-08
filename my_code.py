ed58f8361e5a01f1ddd95162d65b3af99479a3ff
21ccronk <21ccronk@athenian.org>
2020-10-08 17:08:05 +0000

    Close

--- my_code.py
+++ my_code.py
@@ -1,11 +1,10 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #  https://www.geeksforgeeks.org/python-return-statement/
+# Got help from Megan
+# still need to add for loop
+# figure out how to give final total
 
-def pizzashack():
-
-    print("Welcome to the Pizza Shack! Every pizza is served with tomato sauce and cheese, but you can add toppings on top of that, as you'd like, and as your money permits!")
-    print("Every cheese pizza starts at $4. Today's add-on toppings are pepperoni, sausage, olives, mushrooms, bell pepper, and extra cheese!")
-    x = int(input("How much money do you have to spend?: "))
+def pizzashack(x):
 
     toppings = []
     y = 4
@@ -35,13 +34,31 @@ def pizzashack():
             elif topping == 'order':
                 break
     except:
-        print("Please enter a valid topping.")
+        print("Please enter a topping.")
                     
-    print("Your " + str(toppings[:-1]) + " pizza will cost $" + str(y))
+    print("Your cheese, " + str(toppings[:-1]) + " pizza will cost $" + str(y))
     change = x - y
-    return change #why is change not being returned and used 
+    return change
 
+print("Welcome to the Pizza Shack! Every pizza is served with tomato sauce and cheese, but you can add toppings on top of that, as you'd like, and as your money permits!")
+print("Every cheese pizza starts at $4. Today's add-on toppings are pepperoni, sausage, olives, mushrooms, bell pepper, and extra cheese!")
 
-pizzashack() 
-print("You have $" + str(change) + " left.") 
-#again = input("You have $" + str(change) + " left. Would you like to order anither pizza?")
+while True:
+    x = int(input("How much money do you have to spend?: "))
+    change = pizzashack(x)
+    y = x - change
+    if change >= 4:
+        print("You have $" + str(change) + " left.")
+        print("Do you want to order another pizza? (yes/no) ")
+        order2 = input()
+        if order2 == 'no':
+            print("Your pizza will cost $" + str(y) + ".")
+            break
+        if order2 == 'yes':
+            change = pizzashack(change)
+            print("You have $" + str(change) + " left.")
+            break
+    elif change < 4:
+        print("Your pizza will cost $" + str(y) + ".")
+        print("Your change is $" + str(change) + ".")
+        break
